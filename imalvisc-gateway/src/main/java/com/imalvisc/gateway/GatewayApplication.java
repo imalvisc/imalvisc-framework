3d11f1644d52ab73586fcd7c26eac5b967f38544
chenjiaming <13570038865@163.com>
2020-09-27 15:03:00 +0800

    集成gateway

---
+++ imalvisc-gateway/src/main/java/com/imalvisc/gateway/GatewayApplication.java
@@ -0,0 +1,19 @@
+package com.imalvisc.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 程序启动🥱
+ *
+ * @author imalvisc
+ * @version Id: GatewayApplication.java, v 0.1 2020-09-27 14:24 imalvisc Exp $$
+ */
+@SpringBootApplication
+public class GatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class);
+    }
+
+}
